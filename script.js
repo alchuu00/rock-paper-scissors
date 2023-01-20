f9af2db29bd450b1ab58076c099c9a6e2e57fe45
Alja Čekada <alja.cekada@gmail.com>
2023-01-20 19:05:16 +0100

    create playRound function

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 function getComputerChoice() {
-    const moves = Array("rock", "paper", "scissors")
-    const computerChoice = moves[Math.floor(Math.random() * moves.length)]
-    return (computerChoice, moves[computerChoice])
+    const moves = ["rock", "paper", "scissors"];
+    const computerChoice = moves[Math.floor(Math.random() * moves.length)];
+    return computerChoice;
 }
 
-function playerSelection() {
-    const playerChoice = prompt("Input your move (rock, paper or scissors)!")
-    return console.log(playerChoice.toLowerCase())
+function getPlayerChoice() {
+    const playerChoice = prompt("Input your move (rock, paper or scissors)!").toLowerCase();
+    return playerChoice;
 }
 
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection === "rock" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection === "paper") || (playerSelection === "scissors" && computerSelection === "scissors")) {
+        return "It's a Tie!";
+    }
+    else if ((playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")) {
+        return "You lose";
+    }
+    else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")){
+        return "You win";
+    }
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
