187f6e0d5a398d5b8dd7816692bafb447faf0bc4
Alja Čekada <alja.cekada@gmail.com>
2023-01-26 20:12:59 +0100

    create GUI (draft)

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
+// get random computer move
 function getComputerChoice() {
     const moves = ["rock", "paper", "scissors"];
     const computerChoice = moves[Math.floor(Math.random() * moves.length)];
     return computerChoice;
 }
 
-let playerChoice
-function getPlayerChoice() {
-    const elements = document.querySelectorAll('button');
-    elements.forEach(element => {
-      element.addEventListener('click', function() {
-      playerChoice = this.className;
-      });
-    });
-    return playerChoice
-}
-
+// compare computer and player moves and return result
 function playRound(playerSelection, computerSelection) {
     if ((playerSelection === "rock" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection === "paper") || (playerSelection === "scissors" && computerSelection === "scissors")) {
         return "It's a Tie!";
@@ -29,28 +20,68 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game(playerSelection) {
-    const computerSelection = getComputerChoice();
-    console.log(playerSelection)
-    console.log(computerSelection)
+function disableButtons() {
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(element => {
+        element.disabled = true
+    })
+}
 
-    const message = document.querySelector('.message');
-    message.textContent = playRound(playerSelection, computerSelection);
-    console.log(message)
-
-    const computerPoints = document.querySelector('.computerScore');
-    computerPoints.textContent = computerScore
-
-    const playerPoints = document.querySelector('.playerScore');
-    playerPoints.textContent = playerScore
-  }
-
+// global variables
 let playerScore = 0
 let computerScore = 0
+let roundsCounter = 0
 
-const elements = document.querySelectorAll('button');
-elements.forEach(element => {
-  element.addEventListener('click', function() {
-    game(this.className);
-  });
+// start game when button is clicked
+// listen for which of buttons is clicked and make that as player choice
+const cardButtons = document.querySelectorAll('.playerChoice > button');
+cardButtons.forEach(element => {
+    element.addEventListener('click', function () {
+
+        const computerSelection = getComputerChoice();
+        const playerSelection = element.className
+
+        console.log(playerSelection)
+        console.log(computerSelection)
+
+        const message = document.querySelector('.message');
+        message.textContent = playRound(playerSelection, computerSelection);
+        console.log(message)
+
+        const computerPoints = document.querySelector('.computerScore');
+        computerPoints.textContent = computerScore
+
+        const playerPoints = document.querySelector('.playerScore');
+        playerPoints.textContent = playerScore
+
+        // stop the game when one of the players scores 5 points
+        if (playerScore === 5 || computerScore === 5) {
+            const endGame = document.querySelector('.endGame')
+            endGame.textContent = 'GAME OVER'
+            cardButtons.forEach(element => {
+                element.disabled = true;
+            })
+
+            // write the result text
+            const finalScore = document.createElement('div');
+            finalScore.setAttribute('class', 'finalScore');
+            if (playerScore === 5) {
+                finalScore.textContent = 'YOU WON'
+            } else {
+                finalScore.textContent = 'YOU LOSE'
+            }
+            endGame.appendChild(finalScore)
+
+            // restart button
+            const restartButton = document.createElement('button');
+            restartButton.setAttribute('class', 'restartButton');
+            restartButton.textContent = 'RESTART'
+            endGame.appendChild(restartButton)
+
+            restartButton.addEventListener('click', function () {
+                // ADD RESTART FUNCTION
+            })
+        }
+    });
 });
+
